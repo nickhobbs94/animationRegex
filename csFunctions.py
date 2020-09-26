965e6738506d20143b011f4d303617ab474b332b
Nicholas Hobbs <nicholas.hobbs@safegroup.com.au>
2020-09-26 12:23:38 +1000

    instring to camel case and explicitly list functions for eval

--- csFunctions.py
+++ csFunctions.py
@@ -1,6 +1,6 @@
 # we also have the len and str functions which operate just like python's
 
-def inString(searchString, substring):
+def instring(searchString, substring):
   return searchString.find(substring) + 1
 
 def left(s, length):
@@ -21,23 +21,36 @@ def iif(expr, branchTrue, branchFalse):
 def lookup(n, *arg):
   return(arg[n-1])
 
+availableAnimationFunctions = {
+  'instring': instring,
+  'left': left,
+  'right': right,
+  'mid': mid,
+  'iif': iif,
+  'lookup': lookup,
+
+  # these two dont need to be listed I think
+  'len': len,
+  'str': str,
+}
+
 if __name__ == "__main__":
   x="abcdef"
   print(left(x,2) == "ab")
   print(right(x,3) == "def")
   print(mid(x,2,3) == "bcd")
-  print(inString(x,"b") == 2)
-  print(inString(x, 'it was the best of times it was the blurst of times') == 0)
+  print(instring(x,"b") == 2)
+  print(instring(x, 'it was the best of times it was the blurst of times') == 0)
   print(iif(mid(x,2,3) == "bcd", left(x,1),right(x,1)) + '...')
   print(lookup(1, 'x', 'y', 'z'))
 
   # example regex
   # (a|b)c
   # example animation
-  print(inString(x, 'ac') + inString(x,'bc') > 0)
+  print(instring(x, 'ac') + instring(x,'bc') > 0)
   # but what if we wanted to return group 1 from the above?
   # then we'd need something like
-  print(iif(inString(x, 'ac')>0, 'a', iif(inString(x, 'bc')>0, 'b', '')))
+  print(iif(instring(x, 'ac')>0, 'a', iif(instring(x, 'bc')>0, 'b', '')))
 
   # use eval to actually evaluate animations
-  print(eval("left('abc',2)"))
+  print(eval("left('abc',2) + str(len('abc'))", availableAnimationFunctions))
