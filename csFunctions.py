b17751c9cb6b3057424c950000efaf559bf10ced
Nicholas Hobbs <nicholas.hobbs@safegroup.com.au>
2020-09-26 12:14:30 +1000

    Remove pointless try except in instring function, and add regex examples.

--- csFunctions.py
+++ csFunctions.py
@@ -1,8 +1,7 @@
+# we also have the len and str functions which operate just like python's
+
 def inString(searchString, substring):
-  try:
-    return searchString.find(substring) + 1
-  except:
-    return 0
+  return searchString.find(substring) + 1
 
 def left(s, length):
   return s[:length]
@@ -28,5 +27,14 @@ if __name__ == "__main__":
   print(right(x,3) == "def")
   print(mid(x,2,3) == "bcd")
   print(inString(x,"b") == 2)
+  print(inString(x, 'it was the best of times it was the blurst of times') == 0)
   print(iif(mid(x,2,3) == "bcd", left(x,1),right(x,1)) + '...')
   print(lookup(1, 'x', 'y', 'z'))
+
+  # example regex
+  # (a|b)c
+  # example animation
+  print(inString(x, 'ac') + inString(x,'bc') > 0)
+  # but what if we wanted to return group 1 from the above?
+  # then we'd need something like
+  print(iif(inString(x, 'ac')>0, 'a', iif(inString(x, 'bc')>0, 'b', '')))
