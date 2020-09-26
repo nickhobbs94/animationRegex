6b25d55f36f0d001a0e07082e43bea0d60205c36
Nicholas Hobbs <nicholas.hobbs@safegroup.com.au>
2020-09-26 11:59:55 +1000

    Add iif and lookup functions

--- csFunctions.py
+++ csFunctions.py
@@ -13,9 +13,20 @@ def right(s, length):
 def mid(s, start, length):
   return s[start-1:start+length-1]
 
+def iif(expr, branchTrue, branchFalse):
+  if expr == True:
+    return branchTrue
+  else:
+    return branchFalse
+
+def lookup(n, *arg):
+  return(arg[n-1])
+
 if __name__ == "__main__":
   x="abcdef"
   print(left(x,2) == "ab")
   print(right(x,3) == "def")
   print(mid(x,2,3) == "bcd")
   print(inString(x,"b") == 2)
+  print(iif(mid(x,2,3) == "bcd", left(x,1),right(x,1)) + '...')
+  print(lookup(1, 'x', 'y', 'z'))
